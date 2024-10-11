b8cfeca71c36a90164aba52c7a73a9bd6a460d8c
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-10-11 20:29:39 +0200

    done

--- 03tailwindProps/src/main.jsx
+++ 03tailwindProps/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Card from './components/card.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Card username="viktor" post = "rikschef"/>
+    <Card/>
   </StrictMode>,
 )
