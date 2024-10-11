cbc48a6ca63f489fa8b36b989c27d35d0b5f1581
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-10-11 16:29:27 +0200

    counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,15 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [counter, setCounter] = useState(0)
+  
+  //let counter = 69
+
+  const addValue = () => {
+    //setCounter(counter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1) 
+  }
 
   return (
     <>
-      <h1>react course</h1>
-      <h2>counter</h2>
-      <button>add value</button>
-      <button>remove value</button>
-      <p>footer:</p>
+      <h1>react course {counter}</h1>
+      <h2>counter value: {counter}</h2>
+      <button onClick={addValue}>add value</button>
+      <button onClick={removeValue}>remove value</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
