a1288c8aef09202dd737c74b7ec8004f7ead6405
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-10-12 17:05:52 +0200

    more bg buttons

--- 04bgChange/src/App.jsx
+++ 04bgChange/src/App.jsx
@@ -16,10 +16,14 @@ function App() {
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
 
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-          <button onClick={() => setColor('red')} className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>test123</button>
-          <button onClick={() => setColor('green')} className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>test123</button>
-          <button onClick={() => setColor('yellow')}  className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>test123</button>
-          
+          <button onClick={() => setColor('red')} className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>red</button>
+          <button onClick={() => setColor('green')} className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>green</button>
+          <button onClick={() => setColor ('yellow')}  className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>yellow</button>
+          <button onClick={() => setColor ('orange')}  className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>orange</button>
+          <button onClick={() => setColor ('violet')}  className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>violet</button>
+          <button onClick={() => setColor ('pink')}  className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>pink</button>
+          <button onClick={() => setColor ('magenta')}  className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>magenta</button>
+
         </div>
 
       </div>
