b1f64e72f592b0d5dc6d603708ffa58c37d692b2
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-10-12 15:59:25 +0200

    Update App.jsx

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card.jsx'
+import Footer from './components/footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl bg-green-500 p-12 rounded-3xl'>vite with tialwind</h1>
+      <Card username="viktor" post = "rikschef"/>
+      <Card/>
+      <Footer/>
+      <Footer text = " yooo this is da footer"/>
     </>
   )
 }
