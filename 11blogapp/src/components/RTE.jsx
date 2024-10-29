831d25fa420eff1f4c2307bfcd47c9e45376ffc2
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-10-29 20:04:43 +0100

    Header and start RTE

---
+++ 11blogapp/src/components/RTE.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Controller} from "react-hook-form"
+import {Editor} from "@tinymce/tinymce-react"
+
+function RTE() {
+  return (
+    <div>RTE</div>
+  )
+}
+
+export default RTE
