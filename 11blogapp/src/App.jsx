407279001581995d734410676546b07b820265d0
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-10-31 21:50:33 +0100

    Update App.jsx

--- 11blogapp/src/App.jsx
+++ 11blogapp/src/App.jsx
@@ -1,21 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import Header from "./components/Header/Header"
+import Footer from "./components/footer/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+  }, [dispatch])
 
-  return (
-    <>
-      <div>
-        <main>
-          <Outlet/>
-        </main>
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+          <div className="w-full block">
+              <Header/>
+              <main>
+                  <Outlet />
+              </main>
+          </div>
+          <div className="w-full block">
+              <Footer />
+          </div>
       </div>
-    </>
-  )
+  ) : null //updatera till något bättre
 }
 
 export default App
