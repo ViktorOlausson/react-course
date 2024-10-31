20c4fb0940372064e6407c0a769a1eb2bbecb34c
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-10-31 18:42:07 +0100

    Update PostForm.jsx

--- 11blogapp/src/components/post-form/PostForm.jsx
+++ 11blogapp/src/components/post-form/PostForm.jsx
@@ -5,7 +5,7 @@ import Input from "../Input"
 import RTE from '../RTE'
 import Select from "../Select"
 import appwriteService from "../../appwrite/config"
-import {useSelector } from "react-redux"
+import {useSelector} from "react-redux"
 import {useNavigate} from "react-router-dom"
 
 function PostForm({post}) {
@@ -20,11 +20,30 @@ function PostForm({post}) {
 
   const navigate = useNavigate()
   const userData = useSelector((state) => state.auth.userData)
-  const slugTranform = useCallback((value) => {if(value && typeof value === "string") return value.trim().toLowerCase().replace(/[^a-zA-Z\d\s]+/g, '-')
-    .replace(/\s/g, "-")})
+  const slugTranform = useCallback((value) => {if(value && typeof value === "string") return value.trim().toLowerCase()
+    .replace(/[^a-zA-Z\d\s]+/g, '-').replace(/\s/g, "-")},[])
+
+    React.useEffect(() => {
+      watch((value, {name}) => {
+        if(name === "title"){
+          setValue("slug", slugTranform(value.title), {shouldValidate: true})
+        }
+      })
+    }, [watch, slugTranform, setValue])
 
   return (
-    <div>PostForm</div>
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+      <div className="w-2/3 px-2">
+        <Input label="Title" placeholder="Title" className="mb-4" {...register("title", {required: true})}/>
+        <Input label="Slug :" placeholder="Slug" className="mb-4" {...register("slug", {required: true})}
+        onInput={(e) => {setValue("slug", slugTranform(e.currentTarget.value), {shouldValidate: true})}}/>
+        <RTE label="Content: " name="content" control={control} defaultValue={getValues("content")}/>
+      </div>
+
+      <div className='w-1/3 px-2'>
+        <Input label="Featured Image" type="file" className="mb-4" accept="image/png, image/jpg, image/jpeg" {...register("image", {required: true})}/>
+      </div>
+    </form>
   )
 }
 
