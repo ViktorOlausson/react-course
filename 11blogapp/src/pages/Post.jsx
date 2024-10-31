ca3cd4102c3902a6acac3aa8acef07903dbe23cd
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-10-31 21:08:05 +0100

    post

--- 11blogapp/src/pages/Post.jsx
+++ 11blogapp/src/pages/Post.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import appwriteService from "../appwrite/config"
+import Button from "../components/Button"
+import Container from "../components/container/Container"
+import parse from "html-react-parser"
+import {useSelector} from "react-redux"
 
 function Post() {
-  return (
-    <div>Post</div>
-  )
+
+  const [post, setPost] = useState(null)
+  const {slug} = useParams()
+  const navigate = useNavigate()
+  const userData = useSelector((state) => state.auth.userData)
+  const isAuthor = post && userData ? post.userId === userData.$id : false
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => { 
+        if (post) {
+          setPost(post)
+        } else{
+          navigate("/")
+        }
+      })
+    }
+  }, [])
+
+  const deletePost = () => {
+    appwriteService.deletePost(post.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(post.featuredImage)
+        navigate("/")
+      }
+    })
+  }
+  return post ? () : ()
 }
 
 export default Post
