f0efc69da8362c1e5a661937bc66358547ac1c37
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2024-11-01 14:51:37 +0100

    Update RTE.jsx

--- 11blogapp/src/components/RTE.jsx
+++ 11blogapp/src/components/RTE.jsx
@@ -7,7 +7,7 @@ function RTE({name, control, label, defaultValue = ""}) {
     <div className='w-full'>
         {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
         <Controller name={name || "content"} control={control} render={({field:{onChange}}) => (
-            <Editor initialValue={defaultValue} init={{
+            <Editor apiKey='isrpnl7htoqqka6z7itdig3aocf3y41a4g5vqf3cwws55snn' initialValue={defaultValue} init={{
                 height: 500,
                 menubar: false,
                 plugins: ['advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview','anchor', 
